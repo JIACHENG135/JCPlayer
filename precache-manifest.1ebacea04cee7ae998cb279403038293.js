4e4788fdfbe356710643a9315161a1f31d53f919
JIACHENG135 <liulaoye135@tamu.edu>
2020-07-28 16:30:36 -0500

    Add new Home PAge

---
+++ precache-manifest.1ebacea04cee7ae998cb279403038293.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99982544a0d369bf13c050f9f79c4346",
+    "url": "/JCPlayer-Prod/index.html"
+  },
+  {
+    "revision": "6979f519d64475971e53",
+    "url": "/JCPlayer-Prod/static/css/11.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "38195b8b1fd4f0b48c38",
+    "url": "/JCPlayer-Prod/static/css/5.d981d86c.chunk.css"
+  },
+  {
+    "revision": "b975da516e0cae91e522",
+    "url": "/JCPlayer-Prod/static/css/9.7b8de63a.chunk.css"
+  },
+  {
+    "revision": "ee60978ed3ca5cc83d37",
+    "url": "/JCPlayer-Prod/static/js/0.c1d61fef.chunk.js"
+  },
+  {
+    "revision": "53962cc500f28da18a8d",
+    "url": "/JCPlayer-Prod/static/js/1.efa4b1c5.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/JCPlayer-Prod/static/js/1.efa4b1c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1157c2d4bfb9491ed62",
+    "url": "/JCPlayer-Prod/static/js/10.c0d0ee8a.chunk.js"
+  },
+  {
+    "revision": "6979f519d64475971e53",
+    "url": "/JCPlayer-Prod/static/js/11.f107bbeb.chunk.js"
+  },
+  {
+    "revision": "e6368de14161b9b16655",
+    "url": "/JCPlayer-Prod/static/js/12.a3fac8ef.chunk.js"
+  },
+  {
+    "revision": "fd023f869a800287933f",
+    "url": "/JCPlayer-Prod/static/js/13.81294e5e.chunk.js"
+  },
+  {
+    "revision": "72940130bbb034dba5a8",
+    "url": "/JCPlayer-Prod/static/js/14.3a2ae930.chunk.js"
+  },
+  {
+    "revision": "48fb024f6a39fd5e3d6d",
+    "url": "/JCPlayer-Prod/static/js/15.3959d5cc.chunk.js"
+  },
+  {
+    "revision": "08cda9adc06f5d8e1226",
+    "url": "/JCPlayer-Prod/static/js/16.c0c5070a.chunk.js"
+  },
+  {
+    "revision": "0ff8e044cc72f27c0c42",
+    "url": "/JCPlayer-Prod/static/js/17.8f6ce039.chunk.js"
+  },
+  {
+    "revision": "30a9284441b3803ba957",
+    "url": "/JCPlayer-Prod/static/js/18.dc3ea7db.chunk.js"
+  },
+  {
+    "revision": "95c860f7c5114b781b46abadb3ee6e02",
+    "url": "/JCPlayer-Prod/static/js/18.dc3ea7db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fab4732fbc25dca2636",
+    "url": "/JCPlayer-Prod/static/js/19.7fa20f75.chunk.js"
+  },
+  {
+    "revision": "dd5cbebbe5d9c482e9cd",
+    "url": "/JCPlayer-Prod/static/js/2.6afd4e11.chunk.js"
+  },
+  {
+    "revision": "dba666857c668f323f75",
+    "url": "/JCPlayer-Prod/static/js/20.141114a6.chunk.js"
+  },
+  {
+    "revision": "db327da96abb750c362b",
+    "url": "/JCPlayer-Prod/static/js/21.276d99de.chunk.js"
+  },
+  {
+    "revision": "38195b8b1fd4f0b48c38",
+    "url": "/JCPlayer-Prod/static/js/5.c331a2c0.chunk.js"
+  },
+  {
+    "revision": "66d7b7d5032d9bf8dc922edb5f9c7aea",
+    "url": "/JCPlayer-Prod/static/js/5.c331a2c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00cc623b38b5623145a3",
+    "url": "/JCPlayer-Prod/static/js/6.a4600484.chunk.js"
+  },
+  {
+    "revision": "f01b6ee26155721017cc5fc8eb2b8875",
+    "url": "/JCPlayer-Prod/static/js/6.a4600484.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b08fbf6a6c12097d15",
+    "url": "/JCPlayer-Prod/static/js/7.50b74f5d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/JCPlayer-Prod/static/js/7.50b74f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "498c065940f411f5f386",
+    "url": "/JCPlayer-Prod/static/js/8.4e4539c5.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/JCPlayer-Prod/static/js/8.4e4539c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b975da516e0cae91e522",
+    "url": "/JCPlayer-Prod/static/js/9.f6d85703.chunk.js"
+  },
+  {
+    "revision": "19bdb41aefd8320db8c5",
+    "url": "/JCPlayer-Prod/static/js/main.52c0db85.chunk.js"
+  },
+  {
+    "revision": "afe68ac06c48d95595975c9c465c41f8",
+    "url": "/JCPlayer-Prod/static/js/main.52c0db85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64dd6f0578b9f672d010",
+    "url": "/JCPlayer-Prod/static/js/runtime-main.95a64b85.js"
+  },
+  {
+    "revision": "95c26f99d2d74dc2a3bc0a5db9ad9fb8",
+    "url": "/JCPlayer-Prod/static/media/about1.95c26f99.jpg"
+  },
+  {
+    "revision": "00b7b754c2826b02630ccbbe860e8750",
+    "url": "/JCPlayer-Prod/static/media/about2.00b7b754.jpg"
+  },
+  {
+    "revision": "2b466f14430eac13337c9452ff05635c",
+    "url": "/JCPlayer-Prod/static/media/blogPost1.2b466f14.jpg"
+  },
+  {
+    "revision": "f574380de5938344b4dafe04d833270f",
+    "url": "/JCPlayer-Prod/static/media/blogPost2.f574380d.jpg"
+  },
+  {
+    "revision": "8d115020e50d1cdeeb160b7a2f3a88fb",
+    "url": "/JCPlayer-Prod/static/media/blogPost3.8d115020.jpg"
+  },
+  {
+    "revision": "29e641ea808c1389ddc7973e7e6b37c8",
+    "url": "/JCPlayer-Prod/static/media/blogPost4.29e641ea.jpg"
+  },
+  {
+    "revision": "eed51f556c7b96ada363087472035509",
+    "url": "/JCPlayer-Prod/static/media/blogPost5.eed51f55.jpg"
+  },
+  {
+    "revision": "070efa17c85068a75cf78b4e6874a570",
+    "url": "/JCPlayer-Prod/static/media/blogPost6.070efa17.jpg"
+  },
+  {
+    "revision": "d7e9d5a89cc28ab59220f91ba9426fa6",
+    "url": "/JCPlayer-Prod/static/media/details1.d7e9d5a8.jpg"
+  },
+  {
+    "revision": "a48ff9dca413342ff60e1d471c377c41",
+    "url": "/JCPlayer-Prod/static/media/details2.a48ff9dc.jpg"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/JCPlayer-Prod/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/JCPlayer-Prod/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "e69e6026a423d9ec529b57455907b2b1",
+    "url": "/JCPlayer-Prod/static/media/image1.e69e6026.jpg"
+  },
+  {
+    "revision": "8bb0c1c3f829049e059c5af0d0417f64",
+    "url": "/JCPlayer-Prod/static/media/image10.8bb0c1c3.jpg"
+  },
+  {
+    "revision": "bda67d264e2b4b06614c31670c6a8948",
+    "url": "/JCPlayer-Prod/static/media/image3.bda67d26.jpg"
+  },
+  {
+    "revision": "79ebeb1fee81de72e553c9b30632711e",
+    "url": "/JCPlayer-Prod/static/media/image4.79ebeb1f.jpg"
+  },
+  {
+    "revision": "39a17f69e21b46e0cecbf738282960b6",
+    "url": "/JCPlayer-Prod/static/media/image5.39a17f69.jpg"
+  },
+  {
+    "revision": "8b868e181cca8220ca5e725a1f7b984d",
+    "url": "/JCPlayer-Prod/static/media/image6.8b868e18.jpg"
+  },
+  {
+    "revision": "e052cbd6b2db7efc7de674d3047c998c",
+    "url": "/JCPlayer-Prod/static/media/image7.e052cbd6.jpg"
+  },
+  {
+    "revision": "b23293625f3b70e68020633ce1427566",
+    "url": "/JCPlayer-Prod/static/media/image8.b2329362.jpg"
+  },
+  {
+    "revision": "b43804d383d9ba29df937e93cca4f1c1",
+    "url": "/JCPlayer-Prod/static/media/image9.b43804d3.jpg"
+  },
+  {
+    "revision": "0332f20018a3c017a39101d8b0bf46d9",
+    "url": "/JCPlayer-Prod/static/media/search1.0332f200.jpg"
+  },
+  {
+    "revision": "68f46f10bb94215ce2a0bb584e9e4932",
+    "url": "/JCPlayer-Prod/static/media/search2.68f46f10.jpg"
+  }
+]);
